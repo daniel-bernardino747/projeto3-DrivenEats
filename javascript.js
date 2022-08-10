bc59a5b99b438ed6cb40830a635cfaf311660ade
Daniel <dn.danielbernardino@gmail.com>
2022-08-10 17:50:27 -0300

    agr foi

---
+++ javascript.js
@@ -0,0 +1,22 @@
+ // let element = document.getElementById('bottom-label');
+ // 
+ // let element2 = document.getElementById('options');
+ // 
+ // 
+ // function testandoCores(id) {
+ //     let element = document.getElementById(id);
+ // 
+ //     element.style.border = '1px solid red';
+ // }
+ // 
+ // element.addEventListener('click', function() {
+ //     testandoCores(element.id);
+ // });
+ // 
+ // element2.addEventListener('click', function() {
+ //     testandoCores(element2.id);
+ // });
+function myFunction(id) {
+    document.querySelectorAll('button');
+    document.getElementById(id).classList.toggle("border-green");
+}
