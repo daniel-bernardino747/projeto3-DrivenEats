8cc958fbac0c682901f30c86f611a2be4a65562e
Daniel <dn.danielbernardino@gmail.com>
2022-08-12 00:04:12 -0300

    finish purchase conclused

--- javascript.js
+++ javascript.js
@@ -1,16 +1,23 @@
+document.getElementById("bottom-label").disabled = true;
+
 function selector(family, classButton) {
     const selected = document.querySelector(`.${family} .border-green`);
-    console.log(selected)
 
     if (selected !== null) {
         selected.classList.remove("border-green");
     }
 
     const selector = "." + classButton;
-    console.log(selector);
     const button = document.querySelector(selector);
-    console.log(button);
     button.classList.add('border-green');
 
-}
+    const selectedOne = document.querySelector('.first-menu .border-green');
+    const selectedTwo = document.querySelector('.sec-menu .border-green');
+    const selectedThree = document.querySelector('.ter-menu .border-green');
 
+    if (selectedOne !== null && selectedTwo !== null && selectedThree !== null) {
+        const FinishPurchase = document.getElementById('bottom-label');
+        FinishPurchase.innerHTML = 'Fechar pedido';
+        FinishPurchase.classList.add('finishPurchase');
+    }
+}
